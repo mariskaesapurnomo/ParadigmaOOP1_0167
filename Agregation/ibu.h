4173e476d03efe22cb913a5d18af70cd7f055677
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-21 18:32:56 +0700

    membuat object class ibu pada relasi agregasi

--- Agregation/ibu.h
+++ Agregation/ibu.h
@@ -7,22 +7,11 @@ public:
 	vector<anak*> daftar_anak;
 
 	ibu(string pNama) :nama(pNama) {
-		cout << "ibu \"" << nama << "\"ada\n";
+		cout << "Ibu \"" << nama << "\" ada\n";
 	}
 	~ibu() {
-		cout << "ibu \"" << nama << "\" tidak ada\n";
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
 	}
 	void tambahAnak(anak*);
 	void cetakAnak();
 };
-void tambahAnak() {
-	daftar_anak.push_back(pAnak);
-}
-void ibu::cetakAnak() {
-	cout << "Daftar Anak Dari Ibu \"" << this->nama << "\":n";
-	for (auto a : daftar_anak) {
-		cout << a->nama << "/n";
-	}
-	cout << endl;
-}
-#endif
