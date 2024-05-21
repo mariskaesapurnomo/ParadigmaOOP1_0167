c63195eb832129862ab435b1a3d2fa6eded0421a
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-21 10:31:59 +0700

    Add project files.

---
+++ Initialization/Initialization.cpp
@@ -0,0 +1,13 @@
+// Initialization.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main()
+{
+ 
+}
