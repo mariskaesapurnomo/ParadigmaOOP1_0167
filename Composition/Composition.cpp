8944b3f9e65aac5ab327e48d927c2b2dd095c391
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-21 18:13:14 +0700

    class mahasiswa dengan initialization

---
+++ Composition/Composition.cpp
@@ -0,0 +1,17 @@
+// Composition.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "jantung.h"
+#include "manusia.h"
+
+
+int main()
+{
+    manusia* varManusia = new manusia("jono");
+    delete varManusia;
+    return 0;
+}
--- Initialization/Initialization.cpp
+++ Initialization/Initialization.cpp
@@ -5,9 +5,20 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+private:
+	const int id;
+	string nama;
+	float nilai;
 
+public:
+	mahasiswa(int pid, string pnama, float pnilai) : id(pid), nama(pnama), nilai(pnilai) {	//membuat definisi
+	}
 
-int main()
-{
- 
-}
+	~mahasiswa() {
+		cout << "id = " << id << endl;
+		cout << "nama = " << nama << endl;
+		cout << "nilai = " << nilai << endl;
+	}
+};
+
